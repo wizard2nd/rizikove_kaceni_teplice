ab47aeac9a12b12fd293f9e342c9b75caf1e14e4
Jakub Adler <adler.jakub@gmial.com>
2015-06-13 21:10:47 +0100

    work on home page started

--- web/app/themes/rizikove_kaceni_sage_based/functions.php
+++ web/app/themes/rizikove_kaceni_sage_based/functions.php
@@ -28,3 +28,51 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+/*
+    * My custom functions
+    *
+    */
+
+function rk_create_home_image_post_type(){
+    $post_setting = array(
+        'labels' => array(
+            'name' => __('Index Images'),
+            'singular_name' => __('Index Image')
+        ),
+        'public' => true
+    );
+    register_post_type('rk_index_images', $post_setting);
+}
+
+add_action('init', 'rk_create_home_image_post_type');
+
+
+function rk_get_language(){
+
+    $uri = explode('/',trim($_SERVER['REQUEST_URI'],'/'));
+    if (in_array('cz', $uri)) return 'cz';
+    if (in_array('de', $uri)) return 'de';
+
+}
+
+function rk_custom_translate($cz, $de){
+
+    if (rk_get_language() == 'cz') return $cz;
+    if (rk_get_language() == 'de') return $de;
+}
+
+function rk_get_routes(){
+    return $routes = array(4 => 71, 5 => 73, 6 => 75);
+}
+
+function rk_nbsp($string){
+    return str_replace('@', '&nbsp;', $string);
+}
+
+function rk_get_language_url($lang){
+    $current_url = get_permalink();
+    return preg_replace('@/(cz|de)@', "/$lang", $current_url);
+}
+
---
+++ web/app/themes/rizikove_kaceni_sage_based/home_page.php
@@ -0,0 +1,13 @@
+<?php
+/** Template Name: Home Page */
+
+?>
+
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+
+        <? the_content() ?>
+
+    </div><!-- #content -->
+</div><!-- #primary -->
+

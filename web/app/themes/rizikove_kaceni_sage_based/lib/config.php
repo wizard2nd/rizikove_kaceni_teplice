86c4bb07f08bea5c7f8e6f4f076e88f0f570be3a
Jakub Adler <adler.jakub@gmial.com>
2015-09-15 17:16:23 +0100

    Don't display sidebar on profile page

--- web/app/themes/rizikove_kaceni_sage_based/lib/config.php
+++ web/app/themes/rizikove_kaceni_sage_based/lib/config.php
@@ -54,7 +54,7 @@ function display_sidebar() {
       [
         'is_404',
         'is_front_page',
-        ['is_page_template', 'template-custom.php']
+        ['is_page_template', 'profile.php']
       ]
     );
 

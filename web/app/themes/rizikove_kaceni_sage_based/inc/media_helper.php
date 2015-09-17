e1418d793c4b60f971c08770b7f35961d5bf5495
Jakub Adler <adler.jakub@gmial.com>
2015-09-17 19:24:51 +0100

    certificate image dimensions for croping added

--- web/app/themes/rizikove_kaceni_sage_based/inc/media_helper.php
+++ web/app/themes/rizikove_kaceni_sage_based/inc/media_helper.php
@@ -30,6 +30,8 @@ class MediaHelper {
         add_image_size('mobile', 700, 500, array('top', 'left'));
         add_image_size('tablet', 1024, 500, array('top', 'left'));
         add_image_size('desktop', 1024, 500, array('center', 'center'));
+
+        add_image_size('cert-desktop', 690, 1000);
     }
 }
 

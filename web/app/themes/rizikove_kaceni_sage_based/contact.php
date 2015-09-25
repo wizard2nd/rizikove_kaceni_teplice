1ed8374f257fdc804739ee3cf74260d3066aa2e5
Jakub Adler <adler.jakub@gmial.com>
2015-09-25 17:15:44 +0100

    contact page finished

--- web/app/themes/rizikove_kaceni_sage_based/contact.php
+++ web/app/themes/rizikove_kaceni_sage_based/contact.php
@@ -7,27 +7,31 @@ use \rk\contact\Contact;
 
 <div id="contact-page" class="content-area contact-page">
     <div id="content" class="site-content container" role="main">
-        <div class="address-area">
-        <?php
-        $address = new Contact();
+        <div class="row">
+            <div class="address-area">
+                <h1 class="title-bottom-border"><?php _e('Address', 'sage')?></h1>
+                <?php
+                $address = new Contact();
 
-        $address->render_address();
-        $address->render_contact();
+                $address->render_address();
+                $address->render_contact();
+                ?>
+                <div class="map-image">
+                    <?php echo $address->get_map() ?>
+                </div>
+            </div>
+            <div class="contact-form">
+                <h1 class="title-bottom-border"><?php _e('Use this form to contact provider instantly')?></h1>
+                <?php
+                if ( have_posts() ) {
+                    while ( have_posts() ) {
+                        the_post();
+                        the_content();
+                    } // end while
+                } // end if
+                ?>
+            </div>
+        </div>
 
-        ?>
-        </div>
-        <div class="map-image">
-            <?php echo $address->get_map()?>
-        </div>
-        <div class="contact-form">
-            <?php
-            if ( have_posts() ) {
-                while ( have_posts() ) {
-                    the_post();
-                    the_content();
-                } // end while
-            } // end if
-            ?>
-        </div>
     </div><!-- #content -->
 </div><!-- #primary -->

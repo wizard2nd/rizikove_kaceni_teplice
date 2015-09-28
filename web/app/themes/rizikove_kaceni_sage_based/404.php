cae93b134b5f5dc07c0dca1726f97b068300fed8
Jakub Adler <adler.jakub@gmial.com>
2015-09-28 18:37:49 +0100

    404 page

--- web/app/themes/rizikove_kaceni_sage_based/404.php
+++ web/app/themes/rizikove_kaceni_sage_based/404.php
@@ -1,7 +1,9 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<div class="site-content container">
+    <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+    <div class="alert alert-warning">
+      <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+    </div>
+
+    <?php //get_search_form(); ?>
 </div>
-
-<?php get_search_form(); ?>

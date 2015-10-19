f8347f401b16130cf853c0f0a28a89bbd9d6b2a0
Jakub Adler <adler.jakub@gmial.com>
2015-10-19 18:11:25 +0100

    class determinates device added to body class

--- web/app/themes/rizikove_kaceni_sage_based/base.php
+++ web/app/themes/rizikove_kaceni_sage_based/base.php
@@ -9,7 +9,7 @@ use rk\frontend_helper\FrontendHelper;
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
-  <body <?php body_class(FrontendHelper::rk_get_language()); ?>>
+  <body <?php body_class([FrontendHelper::rk_get_language(), FrontendHelper::get_device()]); ?>>
     <!--[if lt IE 9]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>

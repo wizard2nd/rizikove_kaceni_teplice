048df3ed32148c716204a9739d1598b3a431ce73
Jakub Adler <adler.jakub@gmial.com>
2015-10-21 15:52:51 +0100

    time stamp for javascript removed

--- web/app/themes/rizikove_kaceni_sage_based/sage_lib/assets.php
+++ web/app/themes/rizikove_kaceni_sage_based/sage_lib/assets.php
@@ -74,6 +74,6 @@ function assets() {
   }
 
   //wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
-  wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], FrontendHelper::asset_version('scripts/main.js'), true);
+  wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);

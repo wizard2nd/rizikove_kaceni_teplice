f9924869e0dc7ac94bda01f47fc6ea1dbaa29d63
Jakub Adler <adler.jakub@gmial.com>
2015-10-28 17:27:26 +0000

    PageBase used for ajax call to get attachment images

--- web/app/themes/rizikove_kaceni_sage_based/lib/ajax.php
+++ web/app/themes/rizikove_kaceni_sage_based/lib/ajax.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_gallery_attachments_images(){
-\rk\Gallery::get_attachment_images();
+    \rk\PageBase::get_attachment_images();
 }
 
 add_action("wp_ajax_get_gallery_attachments_images", "get_gallery_attachments_images");

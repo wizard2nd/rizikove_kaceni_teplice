86507c26cd51c03b1fb9abe6aeeece05890c6451
Jakub Adler <adler.jakub@gmial.com>
2015-10-28 17:09:46 +0000

    Gallery refactored using Timber templating

--- web/app/themes/rizikove_kaceni_sage_based/gallery.php
+++ web/app/themes/rizikove_kaceni_sage_based/gallery.php
@@ -1,37 +1,10 @@
 <?php
 /** Template Name: Gallery */
 
+use rk\FrontendHelper;
 
+$view['post'] = Timber::get_post();
+$view['spinner'] = FrontendHelper::spinner_path();
+$view['get_images_fn'] = 'get_gallery_attachments_images'; // ajax function to call to get big slider images
 
-?>
-
-<div id="contact-page" class="content-area contact-page">
-    <div id="content" class="site-content container" role="main">
-
-        <?php
-            if (have_posts()){
-                while (have_posts()){
-                    the_post();
-                    the_content();
-                }
-            }
-        ?>
-        <div id="slider-modal-wrap" class="hide-slider slider-modal-wrap" data-get-attachment-images="get_gallery_attachments_images">
-            <div id="close-modal">
-                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-            </div>
-            <img class="loader" src="<?php echo get_stylesheet_directory_uri().'/dist/styles/images/spinner.gif'?>"/>
-            <div class="slider-wrap invisible">
-                <ul class="bx-slider"></ul>
-                <div class="slider-wrap__controls">
-                    <div id="prev-slide" class="control">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    </div>
-                    <div id="next-slide" class="control">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!-- #content -->
-</div><!-- #primary -->
+Timber::render('gallery.twig', $view);

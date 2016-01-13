43dbd6a3aa0f551e715ef3458bad256bb77fb1f7
Jakub Adler <adler.jakub@gmial.com>
2016-01-13 20:30:50 +0000

    Background image disabled on pages other than home page on mobile

--- web/app/themes/rizikove_kaceni_sage_based/templates/header.php
+++ web/app/themes/rizikove_kaceni_sage_based/templates/header.php
@@ -1,44 +1,54 @@
 <header class="banner" role="banner">
-
+        <?php $home_on_mobile = (is_mobile() && is_front_page() && !is_tablet()) ?>
         <nav role="navigation" class="navbar navbar-inverse ">
               <div class="container-fluid center-block menu-center">
+                  <?php if (!$home_on_mobile && !is_tablet() && !is_desktop()) : ?>
+                      <button type="button" class="collapsed menu-logo clearfix" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      </button>
+                      <h1 class="mobile-menu-title"><?php bloginfo('title')?></h1>
+                  <?php else : ?>
                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                       <span class="sr-only">Toggle navigation</span>
                       <span class="icon-bar"></span>
                       <span class="icon-bar"></span>
                       <span class="icon-bar"></span>
                   </button>
-                     <?php
-                        define('THEME_IS_RESPONSIVE', true); // needs to be defined for BootstrapNavWalker class
-                        if (has_nav_menu('primary_navigation')){
-                            wp_nav_menu([
-                                'theme_location'    => 'primary_navigation',
-                                'menu_class'        => 'main-menu nav navbar-nav',
-                                'container'         => 'div',
-                                'container_class'   => 'collapse navbar-collapse main-menu-container',
-                                'container_id'      => 'bs-example-navbar-collapse-1',
-                                'walker'            => new \Webcode\WordPress\Template\BootstrapNavWalker()
-                            ]);
-                        }
-                     ?>
+                  <?php endif ?>
+                  <?php
+                     define('THEME_IS_RESPONSIVE', true); // needs to be defined for BootstrapNavWalker class
+                     if (has_nav_menu('primary_navigation')){
+                        wp_nav_menu([
+                            'theme_location'    => 'primary_navigation',
+                            'menu_class'        => 'main-menu nav navbar-nav',
+                            'container'         => 'div',
+                            'container_class'   => 'collapse navbar-collapse main-menu-container',
+                            'container_id'      => 'bs-example-navbar-collapse-1',
+                            'walker'            => new \Webcode\WordPress\Template\BootstrapNavWalker()
+                        ]);
+                     }
+                  ?>
               </div>
         </nav>
-        <div class="header-image">
-            <?php if (is_mobile() || is_tablet()) : ?>
-            <div class="mobile">
-                <div class="site-logo-title-wrap">
-                    <div class="site-logo"></div>
-                    <h1 class="site-title"><?php bloginfo('title') ?></h1>
+        <div class="header-image <?php if (!$home_on_mobile && !is_desktop() && !is_tablet()) echo 'hide-header'; ?>">
+            <?php if ($home_on_mobile || is_tablet()) : ?>
+                <div class="mobile">
+                    <div class="site-logo-title-wrap">
+                        <div class="site-logo"></div>
+                        <h1 class="site-title"><?php bloginfo('title') ?></h1>
+                    </div>
+                    <h2 class="site-description"><?php bloginfo('description') ?></h2>
                 </div>
-                <h2 class="site-description"><?php bloginfo('description') ?></h2>
-            </div>
+            <?php elseif (is_mobile()) : ?>
+
             <?php else : ?>
                 <div class="desktop">
                     <div class="site-logo-title-wrap">
-                        <div class="site-logo"></div>
-                        <div class="title-description-wrap">
-                            <h1 class="site-title"><?php bloginfo('title') ?></h1>
-                            <h2 class="site-description"><?php bloginfo('description') ?></h2>
+                        <div class="title-background">
+                            <div class="title-description-wrap">
+                                <div class="site-logo"></div>
+                                <h1 class="site-title"><?php bloginfo('title') ?></h1>
+                                <h2 class="site-description"><?php bloginfo('description') ?></h2>
+                            </div>
                         </div>
                     </div>
                 </div>

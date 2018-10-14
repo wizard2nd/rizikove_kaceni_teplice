5056dd6a9f37adb6fee8a92f8be034f07ecea984
Jakub Adler <adler.jakub@gmial.com>
2018-10-14 20:28:26 +0100

    services in four blocks

--- web/app/themes/rizikove_kaceni_sage_based/home_page.php
+++ web/app/themes/rizikove_kaceni_sage_based/home_page.php
@@ -5,7 +5,7 @@ use rk\FrontendHelper;
 ?>
 
 <div id="home-page" class="content-area">
-    <div id="content" class="site-content container home-page" role="main">
+    <div id="content" class="home-page" role="main">
         <?php $index_pages = array(
             36 => __('Risk Felling', 'sage'),
             38 => __('Pruning trees', 'sage'),
@@ -13,48 +13,38 @@ use rk\FrontendHelper;
             42 => __('Forklift trim', 'sage')
         );
         ?>
+        <section id="service-list" class="service-list">
 
-            <div id="home-page-carousel" class="carousel slide" data-ride="carousel" data-interval="4000" data-pause="hover">
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <?php $i = 0 ?>
-                    <?php foreach($index_pages as $page_id => $title) : ?>
-                        <?php
-                            $active = $i++ == 0 ? 'active': null;
-                            $page_url = get_page_link($page_id);
-                            $long_title = strlen($title) > 20 ? 'long-title' : null;
-                        ?>
-                        <div class="item <?php echo $active ?>">
-                            <a href="<?php echo $page_url?>">
-                                <?php //$page = get_post($page_id) ?>
-                                <?php echo FrontendHelper::get_thumbnail_image_by_device($page_id)?>
-                                <a class="bottom-title <?= $long_title?>" href="<?php echo $page_url ?>">
-                                    <h1><?php echo $title ?></h1>
-                                    <div class="more-info"><span class="more-info__text"><? _e('More info', 'sage')?></span><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></div>
-                                </a>
-                            </a>
+            <div class="row">
+                <?php foreach ($index_pages as $page_id => $title) : ?>
+                    <div class="col-md-3 service-list__item">
+                        <?php echo FrontendHelper::get_thumbnail_image_by_device($page_id) ?>
+                        <div class="title">
+                            <?= $title ?>
                         </div>
-                    <?php endforeach ?>
-                </div>
+                        <div class="short-description">
+                            Lorem Ipsum .....
+                        </div>
 
-                <!-- Controls -->
-                <a class="left carousel-control" href="#home-page-carousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#home-page-carousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                        <a href="<?php echo get_page_link($page_id) ?>">
+                            <div class="more-info">
+                                <span class="more-info__text"><? _e('More info', 'sage') ?></span>
+                                <span class="glyphicon glyphicon-menu-right" aria-hidden="true">
+                            </div>
+                        </a>
+                    </div>
+                <?php endforeach ?>
             </div>
-        <div class="home-page-post">
-            <?php
-                if (have_posts()){
+
+            <div class="home-page-post">
+                <?php
+                if (have_posts()) {
                     echo FrontendHelper::add_icon_to_list_item($post->post_content);
                 }
-            ?>
-        </div>
+                ?>
+            </div>
 
+        </section>
     </div><!-- #content -->
 </div><!-- #primary -->
 

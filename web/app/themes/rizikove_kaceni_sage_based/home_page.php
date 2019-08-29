3ae77ee714cdd3e460fb839a3282d06c03d10697
Jakub Adler <adler.jakub@gmail.com>
2019-08-29 22:33:02 +0100

    Front page section divider (Services)

--- web/app/themes/rizikove_kaceni_sage_based/home_page.php
+++ web/app/themes/rizikove_kaceni_sage_based/home_page.php
@@ -3,6 +3,12 @@
 use rk\FrontendHelper;
 
 $view = [];
+$services_ids = array(36, 38, 42);
+
+foreach ($services_ids as $service_id) {
+    $view['services']['img'][$service_id] = FrontendHelper::get_thumbnail_image_by_device($service_id);
+    $view['services']['post'][$service_id] = new Timber\Post($service_id);
+}
 
 Timber::render('home_page.twig', $view);
 

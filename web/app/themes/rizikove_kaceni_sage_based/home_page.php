52fd4071e065a8600f2810e5a7a4200d0b427b7f
Jakub Adler <adler.jakub@gmail.com>
2019-08-30 17:23:54 +0100

    Display front services on mobile

--- web/app/themes/rizikove_kaceni_sage_based/home_page.php
+++ web/app/themes/rizikove_kaceni_sage_based/home_page.php
@@ -6,8 +6,12 @@ $view = [];
 $services_ids = array(36, 38, 42);
 
 foreach ($services_ids as $service_id) {
-    $view['services']['img'][$service_id] = FrontendHelper::get_thumbnail_image_by_device($service_id);
-    $view['services']['post'][$service_id] = new Timber\Post($service_id);
+    $service = [
+        'image' => get_the_post_thumbnail($service_id, 'gallery-thumb'),
+        'title' => get_the_title($service_id),
+        'link' => get_permalink($service_id)
+    ];
+    $view['services'][$service_id] = $service;
 }
 
 Timber::render('home_page.twig', $view);

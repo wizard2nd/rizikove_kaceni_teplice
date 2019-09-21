25661ec78c7c01a855be117af0db6043fb559b3f
Jakub Adler <adler.jakub@gmail.com>
2019-09-21 19:28:22 +0100

    Service module (css, twig template)

--- web/app/themes/rizikove_kaceni_sage_based/home_page.php
+++ web/app/themes/rizikove_kaceni_sage_based/home_page.php
@@ -6,9 +6,9 @@ $services_ids = array(36, 38, 42);
 
 foreach ($services_ids as $service_id) {
     $service = [
-        'image' => get_the_post_thumbnail($service_id, 'gallery-thumb'),
+        'image_src' => get_the_post_thumbnail_url($service_id, 'gallery-thumb'),
         'title' => get_the_title($service_id),
-        'link' => get_permalink($service_id)
+        'url' => get_permalink($service_id)
     ];
     $view['services'][$service_id] = $service;
 };

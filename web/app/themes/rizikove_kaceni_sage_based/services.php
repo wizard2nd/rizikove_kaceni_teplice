3fa63cfb116b5b612c423040864f6a077cada3fd
Jakub Adler <adler.jakub@gmail.com>
2019-09-21 21:30:38 +0100

    Display all child services on Service page

--- web/app/themes/rizikove_kaceni_sage_based/services.php
+++ web/app/themes/rizikove_kaceni_sage_based/services.php
@@ -3,15 +3,10 @@
 use rk\FrontendHelper;
 use rk\Service;
 
-$service_ids = [36, 38, 40, 42];
-
-$services = [];
-foreach ($service_ids as $service_id) {
-    $service = new Service($service_id, [
-        'content',
-        'featured_images'
-    ]);
-    $services[$service_id] = $service->display_fields();
+$services = get_children(array('post_parent' => get_the_ID()));
+foreach ($services as $service) {
+    $service = new Service($service);
+    $services[$service->id] = $service->display_fields();
 }
 
 $view['spinner'] = FrontendHelper::spinner_path();

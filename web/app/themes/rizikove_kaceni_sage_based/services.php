5ba469d3ec5c8032f903ea49aebba7b5c8b58639
Jakub Adler <adler.jakub@gmail.com>
2019-10-20 23:27:51 +0100

    Display services in specific order

--- web/app/themes/rizikove_kaceni_sage_based/services.php
+++ web/app/themes/rizikove_kaceni_sage_based/services.php
@@ -3,9 +3,10 @@
 use rk\FrontendHelper;
 use rk\Service;
 
-$services = get_children(array('post_parent' => get_the_ID()));
-foreach ($services as $service) {
-    $service = new Service($service);
+$service_ids = [36, 38, 42, 40, 1161];
+$services = [];
+foreach ($service_ids as $service_id) {
+    $service = new Service($service_id);
     $services[$service->id] = $service->display_fields();
 }
 
